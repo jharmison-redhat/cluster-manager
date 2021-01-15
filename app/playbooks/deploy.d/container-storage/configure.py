09c4c776668d44e7f09f59a6a49ca87d06e44d45
James Harmison <jharmison@redhat.com>
2021-01-15 10:33:08 -0500

    Temporarily defaulted to auto-detection of drives, reworked container storage to pull from drives appropriately

--- app/playbooks/deploy.d/container-storage/configure.py
+++ app/playbooks/deploy.d/container-storage/configure.py
@@ -1,104 +1,62 @@
-import os
-import sys
-import pickle
-import yaml
+#!/usr/bin/env python
 from collections import defaultdict
-import ansible.parsing.yaml.objects
-import ansible.utils.unsafe_proxy
-from PyInquirer import Token, prompt, Separator
+import os
+import yaml
 
 
-def checkbox(prompt_label, options, cache=None):
-    # ask for input
-    cached_data = []
-    if cache:
-        try:
-            cached_data = pickle.load(open(cache, 'rb'))
-        except (IOError, EOFError):
-            pass
-
-    if type(options) == list:
-        choices = [{'name': item,
-                    'checked': item in cached_data} for item in options]
-    else:
-        choices = []
-        for item in options.keys():
-            item = str(item)
-            choices += [Separator(item)]
-            choices += [{'name': f'{item}: {option}',
-                         'checked': f'{item}: {option}' in cached_data}
-                        for option in options[item]]
-    questions = [
-        {
-            'type': 'checkbox',
-            'message': str(prompt_label),
-            'name': 'results',
-            'choices': choices
-        }
-    ]
-    result = prompt(questions)['results']
-    result.sort()
-
-    # update cache
-    if cache and result != cached_data:
-        pickle.dump(result, open(cache, 'wb'))
-
-    # nice format results
-    if type(options) != list:
-        nice_result = defaultdict(list)
-        for key, val in [item.split(': ') for item in result]:
-            nice_result[key].append(val)
-        return nice_result
-
-    # return results to ansible
-    return result
+drives_file = os.environ.get('DRIVES_FILE', '/data/drives.yml')
 
 
-def load_stats(stats_file):
-    return yaml.load(open(stats_file, 'r'), Loader=yaml.FullLoader)
-
-
-def save_stats(stats_file, stats_data):
-    yaml.dump(stats_data, open(stats_file, 'w'))
-
-
-def main():
-    # load data
-    stats = load_stats(os.environ['STATS_FILE'])
-
-    # ask for storage nodes
-    stg_nodes = checkbox('Select nodes to use for storage',
-                         stats['cluster_nodes'],
-                         '/data/container-storage-nodes.cache')
-    stg_nodes = [str(item) for item in stg_nodes]
-
-    # ask for storage devices
-    drives = dict(zip(stg_nodes,
-                      [stats['cluster_drives'][item]
-                       for item in stg_nodes]))
-    for key in drives.keys():
-        drives[key].sort()
-    stg_drives = checkbox('Select the block devices to use for storage',
-                          drives,
-                          '/data/container-storage-drives.cache')
-    stg_drives_out = []
-    for key, val in stg_drives.items():
-        stg_drives_out.append({'host': key, 'drives': val})
-
-    # count drives per node
-    drives_per_node = min([len(item['drives']) for item in stg_drives_out])
-
-    # save data
-    save_stats(os.environ['STATS_FILE'], {
-        'stg_nodes': stg_nodes,
-        'stg_drives': stg_drives_out,
-        'cluster_nodes': stats['cluster_nodes'],
-        'cluster_drives': stats['cluster_drives'],
-        'drives_per_node': drives_per_node
-    })
-
+def return_zero():
     return 0
 
 
-if __name__ == '__main__':
-    sys.exit(main())
+def good_fit(drive):
+    if drive.get('type') == 'disk' and drive.get('children') is None:
+        return True
+    else:
+        return False
+
+
+with open(drives_file) as f:
+    data = yaml.safe_load(f)
+
+drives = data.get('drive_data')
+use_drives = data.get('use_drives')
+
+if use_drives is None:
+    possible_drives = {}
+    drive_size_count = defaultdict(return_zero)
+    for node in drives:
+        possible_drives[node] = []
+        for drive in drives[node]:
+            if good_fit(drive):
+                possible_drives[node].append({
+                    'path': '/dev/{}'.format(drive.get('name')),
+                    'size': drive.get('size')
+                })
+                drive_size_count[drive.get('size')] += 1
+
+    most_common_size = max(drive_size_count,
+                           key=lambda x: drive_size_count[x])
+
+    use_drives = {}
+    drives_per_node = 0
+    for node in possible_drives:
+        use_drives[node] = []
+        for drive in possible_drives[node]:
+            if drive['size'] == most_common_size:
+                use_drives[node].append(drive['path'])
+        use_drives[node].sort()
+        if drives_per_node == 0:
+            drives_per_node = len(use_drives[node])
+        elif drives_per_node != len(use_drives[node]):
+            raise RuntimeError(('The number of detected drives per node is not'
+                                ' consistent! Please check drives.yml.'))
+
+    with open(drives_file, 'w') as f:
+        yaml.safe_dump({
+            'drive_data': drives,
+            'use_drives': use_drives,
+            'drives_per_node': drives_per_node
+        }, f)
--- app/playbooks/deploy.d/container-storage/main.sh
+++ app/playbooks/deploy.d/container-storage/main.sh
@@ -2,7 +2,8 @@
 
 ME=$(dirname $0)
 
-STATS_FILE=/tmp/pipeline ansible-playbook $ME/gather-facts.yml $@ || exit 1
-STATS_FILE=/tmp/pipeline python3 $ME/configure.py $@ || exit 1
-ansible-playbook $ME/local-storage.yml -e @/tmp/pipeline $@ || exit 1
-ansible-playbook $ME/container-storage.yml -e @/tmp/pipeline $@ || exit 1
+[ -f /data/drives.yml ] || detect drives
+python3 $ME/configure.py $@ || exit 1
+
+ansible-playbook $ME/local-storage.yml $@ || exit 1
+ansible-playbook $ME/container-storage.yml $@ || exit 1
